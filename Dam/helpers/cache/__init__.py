87e3c178dc24e56ccf6fdeb4c1189b6fc0b90e97
GentaProject5 <117004544+GentaProject5@users.noreply.github.com>
2022-11-03 19:52:23 +0700

    Update __init__.py

--- Dam/helpers/cache/__init__.py
+++ Dam/helpers/cache/__init__.py
@@ -3,5 +3,5 @@ from os import getenv
 
 CBHDSYS = getenv(
     "CBHDSYS",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2RhbXN5eC9EYW0tVXNlcmJvdA==").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL0dlbnRhUHJvamVjdDUvVXNib3REYW0=").decode("utf-8"),
 )
